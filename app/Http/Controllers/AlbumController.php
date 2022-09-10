057a8b39242e7bcf0b9c401e1b0a7919db0de599
VietrovSO <ipz20bd-203@stu.zsea.edu.ua>
2022-09-10 16:31:48 +0300

    Added controllers for album and genre. Created database dump

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Album;
+use App\Models\Genre;
+use App\Models\Author;
 use Illuminate\Support\Facades\Log;
 
 class AlbumController extends Controller
 {
-
     function show($id){
-        Log::info('Album id:', ['id' => $id]);
-        return view('albums/album', ['album' => Album::findOrFail($id)]);
+        $album = Album::findOrFail($id);
+        $author = Author::findOrFail($album->author_id);
+        return view('albums/album', 
+            [
+                'album' => $album,
+                'author' => $author,
+            ]
+        );
+
     }
 }
--- resources/views/albums/album.blade.php
+++ resources/views/albums/album.blade.php
@@ -4,5 +4,6 @@
     <h1>{{$album->title}}</h1>
 
     <p>{{$album->release_date}}</p>
+    <h3>{{$author->name}}</h3>
 
 @endsection
